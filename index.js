1e7ab1d076d2757194a1678ca2ab44a14e70e238
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-10-28 12:46:42 -0500

    worked on giving them a token

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-require('dotenv').config();
+require("dotenv").config();
 
 const PORT = 3000;
 const express = require("express");
 const server = express();
 
-const apiRouter = require("./api");
-server.use("/api", apiRouter);
-
 const morgan = require("morgan");
 server.use(morgan("dev"));
 
 server.use(express.json());
+
+const apiRouter = require("./api");
+server.use("/api", apiRouter);
 
 server.use((req, res, next) => {
   console.log("<____Body Logger START____>");
