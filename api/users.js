d2ee32609388728eeb7c5d66b580f3135289e664
hrbaldwin <haileyrebeccabaldwin@gmail.com>
2022-10-28 13:42:15 -0500

    wrapped up part 2 of part 2

--- api/users.js
+++ api/users.js
@@ -1,11 +1,11 @@
-const jwt = require('jsonwebtoken');
+const jwt = require("jsonwebtoken");
 const express = require("express");
 const usersRouter = express.Router();
 const { JWT_SECRET } = process.env;
 
-const { getAllUsers, getUserByUsername } = require("../db");
+const { getAllUsers, getUserByUsername, createUser } = require("../db");
 
-console.log()
+console.log();
 usersRouter.use((req, res, next) => {
   console.log("A request is being made to /users");
 
@@ -23,29 +23,69 @@ usersRouter.get("/", async (req, res) => {
 usersRouter.post("/login", async (req, res, next) => {
   const { username, password } = req.body;
 
-  if(!username || !password){
+  if (!username || !password) {
     next({
-        name: "MissingCredentialsError",
-        message: "Please supply both a username and password"
+      name: "MissingCredentialsError",
+      message: "Please supply both a username and password",
     });
   }
 
   try {
     const user = await getUserByUsername(username);
 
-    if(user && user.password == password){
-        const token = jwt.sign({id: user.id, username}, JWT_SECRET);
-        res.send({ message: "you're logged in!"});
-        return token
-    }else{
-        next({
-            name: 'IncorrectCredentialsError', 
-            message: 'Username or password is incorrect'
-        })
+    if (user && user.password == password) {
+      const token = jwt.sign({ id: user.id, username }, JWT_SECRET);
+      res.send({ message: "you're logged in!", token });
+      return token;
+    } else {
+      next({
+        name: "IncorrectCredentialsError",
+        message: "Username or password is incorrect",
+      });
     }
   } catch (error) {
     console.log(error);
     next(error);
+  }
+});
+
+usersRouter.post("/register", async (req, res, next) => {
+  const { username, password, name, location } = req.body;
+
+  try {
+    const _user = await getUserByUsername(username);
+
+    if (_user) {
+      next({
+        name: "UserExistsError",
+        message: "A user by that username already exists",
+      });
+    }
+
+    const user = await createUser({
+      username,
+      password,
+      name,
+      location,
+    });
+
+    const token = jwt.sign(
+      {
+        id: user.id,
+        username,
+      },
+      process.env.JWT_SECRET,
+      {
+        expiresIn: "1w",
+      }
+    );
+
+    res.send({
+      message: "thank you for signing up",
+      token,
+    });
+  } catch ({ name, message }) {
+    next({ name, message });
   }
 });
 
